d33f6425459525bd1b9d3ace14c636ba90ed82e0
Alain Papazoglou <alain@kredeum.com>
2021-05-28 18:55:47 +0200

    adjustments

--- thegraph/queries/index.mjs
+++ thegraph/queries/index.mjs
@@ -1,20 +1,24 @@
-import axios from "axios";
+import fetch from "node-fetch";
 import fs from "fs";
 
 async function graphQL(url, query) {
   let response;
   try {
-    response = await axios.post(url, { query: query });
+    response = await fetch(url, { 
+      method: 'POST',
+      body: JSON.stringify({query}),
+      headers: { 'Content-Type': 'application/json' }
+      });
   } catch (error) {
     console.error(error);
   }
   return response;
 }
 
-const queryFile = process.argv[2] || "nft.gql";
-const url = process.argv[3] || "https://api.thegraph.com/subgraphs/name/zapaz/kredeum-nft";
+const queryFile = process.argv[2] || "contracts.gql";
+const url = process.argv[3] || "https://api.thegraph.com/subgraphs/name/zapaz/eip721-mumbai";
 const query = fs.readFileSync(queryFile, "utf8");
 
-graphQL(url, query).then((res) => {
-  console.log(JSON.stringify(res.data, null, "  "));
-});
+graphQL(url, query)
+    .then(res => res.json())
+    .then(json =>console.log(JSON.stringify(json, null,2)));
